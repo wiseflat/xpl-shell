e3209da3cefe20b32ad5a508ad7d387a50b734e2
Mathieu Garcia <mgarcia@wiseflat.com>
2015-01-09 23:06:35 +0100

    Upgrade core

--- index.js
+++ index.js
@@ -4,22 +4,30 @@ var wt = new xplshell(null, {
 	//xplSource: 'bnz-shell.wiseflat'
 });
 
-wt._init(function(error, xpl) { 
+wt.init(function(error, xpl) { 
 
 	if (error) {
 		console.error(error);
 		return;
 	}
         
+        // Load config file into hash
         wt.readConfig();
-                
-        xpl.on("xpl:shell.config", function(evt) {
-                if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
-        }); 
+        wt.readBasic();
+        
+        // Send every minutes an xPL status message 
+        setInterval(function(){
+                wt.sendConfig();
+                wt.sendBasic();
+        }, 60 * 1000);
 
         xpl.on("xpl:shell.request", function(evt) {
-                if(evt.headerName == 'xpl-cmnd') wt.readConfig();
+                if(evt.headerName == 'xpl-cmnd') wt.sendConfig();
         });
+        
+        /*xpl.on("xpl:shell.config", function(evt) {
+                if(evt.headerName == 'xpl-cmnd' && wt.validConfigSchema(evt.body)) wt.writeConfig(evt.body);
+        });*/
 
         xpl.on("xpl:shell.basic", function(evt) {
                 if(evt.headerName == 'xpl-cmnd' && wt.validBasicSchema(evt.body)) wt.sendCommand(evt.body);
